1efe91e5c0377bf86833e280d34493a02476d5e4
hyatt <hyatt@hyatt.(none)>
2014-07-22 19:30:44 -0400

    Added lots of test files. Not debugged yet.

--- indradrive_hw_iface/src/cs_robot_hw.cpp
+++ indradrive_hw_iface/src/cs_robot_hw.cpp
@@ -8,8 +8,7 @@ IndradriveCSRobotHW::IndradriveCSRobotHW(ros::NodeHandle& nh, std::string& joint
 {
   // Register joint interface handle
   jnt_state_iface_.registerHandle(
-      JointStateHandle(joint_name, &pos_act_, &vel_act_, 
-                                       &eff_act_));
+      JointStateHandle(joint_name, &pos_act_, &vel_act_, &eff_act_));
   jnt_vel_iface_.registerHandle(
       JointHandle(jnt_state_iface_.getHandle(joint_name), &vel_cmd_));
 
@@ -20,6 +19,7 @@ IndradriveCSRobotHW::IndradriveCSRobotHW(ros::NodeHandle& nh, std::string& joint
 
 void IndradriveCSRobotHW::init()
 {
+  pos_act_ = 0.0; vel_act_ = 0.0; eff_act_ = 0.0; vel_cmd_ = 0.0;
 }
 
 void IndradriveCSRobotHW::read()
@@ -28,6 +28,8 @@ void IndradriveCSRobotHW::read()
 
 void IndradriveCSRobotHW::write()
 {
+  pos_act_ += vel_cmd_;
+  vel_act_ = vel_cmd_;
 }
 
 IndradriveCSRobotHW::~IndradriveCSRobotHW()
---
+++ indradrive_hw_iface/test/test_trajectory_ctrl.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import copy
+import sys
+
+from trajectory_msgs.msg import JointTrajectoryPoint
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from std_msgs.msg import Float64MultiArray
+import rospy
+import actionlib
+
+def main():
+    rospy.init_node("test_traj_ctrl")
+    act_cli = actionlib.SimpleActionClient(
+            '/vel_trajectory_ctrl/follow_joint_trajectory', 
+            FollowJointTrajectoryAction)
+    if not act_cli.wait_for_server(rospy.Duration.from_sec(1.)):
+        rospy.logerr("Can't find action server")
+        return
+
+    jtp1 = JointTrajectoryPoint()
+    jtp1.positions = [0.0]
+    jtp1.velocities = [0.0]
+    jtp1.accelerations = [0.0]
+    jtp1.time_from_start = rospy.Duration.from_sec(5.)
+
+    jtp2 = JointTrajectoryPoint()
+    jtp2.positions = [0.3]
+    jtp2.velocities = [0.1]
+    jtp2.accelerations = [0.0]
+    jtp2.time_from_start = rospy.Duration.from_sec(10.)
+
+    jtp3 = JointTrajectoryPoint()
+    jtp3.positions = [0.1]
+    jtp3.velocities = [0.0]
+    jtp3.accelerations = [0.0]
+    jtp3.time_from_start = rospy.Duration.from_sec(15.)
+
+    fjt = FollowJointTrajectoryGoal()
+    fjt.trajectory.header.stamp = rospy.Time.now()
+    fjt.trajectory.joint_names = ['rail_joint']
+    fjt.trajectory.points = [jtp1, jtp2, jtp3]
+
+    act_cli.send_goal(fjt)
+    rospy.loginfo("Starting trajectory")
+    rospy.sleep(0.1)
+    act_cli.wait_for_result()
+    rospy.loginfo("Trajectory complete")
+
+if __name__ == "__main__":
+    main()
