302de440f0d3556206317052993342c9b208ee93
hyatt <kphawkins@gmail.com>
2014-08-13 16:02:29 -0400

    Added realtime xenomai control methods

--- indradrive_hw_iface/src/vel_ctrl_man.cpp
+++ indradrive_hw_iface/src/vel_ctrl_man.cpp
@@ -3,10 +3,54 @@
 #include <controller_manager/controller_manager.h>
 #include <indradrive_hw_iface/vel_ec_ctrl.h>
 
-using namespace indradrive;
+#ifdef XENOMAI_REALTIME
+
+#include <sys/mman.h>
+#include <rtdm/rtdm.h>
+#include <native/task.h>
+#include <native/sem.h>
+#include <native/mutex.h>
+#include <native/timer.h>
+#include <rtdk.h>
+#include <pthread.h>
+
+RT_TASK update_loop_task_info;
+
+void signal_handler(int sig)
+{
+  stop_requested = true;
+}
+
+#endif
+
+typedef indradrive::VelocityEthercatController VelEcatCtrl;
+VelEcatCtrl *cs_hw_ptr;
+controller_manager::ControllerManager *cm_ptr;
+bool stop_requested;
+
+void update_loop_task(void *arg)
+{
+  ros::Duration period(1.0/1000.0);
+#ifdef XENOMAI_REALTIME
+	rt_task_set_periodic(NULL, TM_NOW, 1000000); // ns
+#else
+  ros::Rate r(1000.0);
+#endif
+  while (ros::ok() && !stop_reqeusted) {
+#ifdef XENOMAI_REALTIME
+		rt_task_wait_period(NULL);
+#else
+    r.sleep();
+#endif
+    cs_hw_ptr->read();
+    cm_ptr->update(ros::Time::now(), period);
+    cs_hw_ptr->write();
+  }
+}
 
 int main(int argc, char** argv)
 {
+  stop_reqeusted = false;
   ros::init(argc, argv, "cs_controller_man");
 
   ros::AsyncSpinner spinner(1);
@@ -18,20 +62,47 @@ int main(int argc, char** argv)
   std::string joint_name;
   nh_priv.param<std::string>("joint_name", joint_name, "indradrive_cs_joint");
 
-  VelocityEthercatController cs_hw(nh, nh_priv, joint_name);
-  if(cs_hw.init()) {
+  cs_hw_ptr = new VelEcatCtrl(nh, nh_priv, joint_name);
+  if(cs_hw_ptr->init()) {
     printf("Failed to initialize controller\n");
     return -1;
   }
 
-  controller_manager::ControllerManager cm(&cs_hw, nh);
+  cm_ptr = new controller_manager::ControllerManager(cs_hw_ptr, nh);
 
-  ros::Duration period(1.0/1000.0);
-  ros::Rate r(1000.0);
-  while (ros::ok()) {
-    cs_hw.read();
-    cm.update(ros::Time::now(), period);
-    cs_hw.write();
-    r.sleep();
+#ifdef XENOMAI_REALTIME
+  int ret;
+  rt_print_auto_init(1);
+
+  signal(SIGTERM, signal_handler);
+  signal(SIGINT, signal_handler);
+
+  mlockall(MCL_CURRENT | MCL_FUTURE);
+
+  ret = rt_task_create(&update_loop_task_info, "update_loop", 0, 80, T_FPU);
+  if (ret < 0) {
+    fprintf(stderr, "Failed to create task: %s\n", strerror(-ret));
+    return -1;
   }
+
+  printf("Starting update_loop_task_info...\n");
+  ret = rt_task_start(&update_loop_task_info, &update_loop_task, NULL);
+  if (ret < 0) {
+    fprintf(stderr, "Failed to start task: %s\n", strerror(-ret));
+    return -1;
+  }
+
+  while (ros::ok() && !stop_requested) {
+    sched_yield();
+  }
+
+  printf("Deleting realtime task...\n");
+  rt_task_delete(&my_task);
+
+#else
+  update_loop_task();
+#endif
+
+  delete cm_ptr;
+  delete cs_hw_ptr;
 }
