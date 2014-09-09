3631f1cf41c7dffb6e549637ae5f95c2670f218b
menchi <kphawkins@gmail.com>
2014-09-09 14:42:37 -0400

    BG sub test

--- robot_pc_filter/src/pc_cylinder_test.cpp
+++ robot_pc_filter/src/pc_cylinder_test.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <algorithm>
 
 #include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
@@ -12,12 +13,24 @@
 #include <Eigen/Geometry>
 #include <Eigen/SVD>
 
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+
 #include <sensor_msgs/PointCloud2.h>
 
 using namespace std;
+using namespace boost::accumulators;
+using namespace boost::math;
 
 typedef pcl::PointXYZRGB PRGB;
 typedef pcl::PointCloud<PRGB> PCRGB;
+
+typedef accumulator_set<float, stats<tag::mean,tag::variance,tag::count> > mean_var_acc;
+
+#define MAX_Z 6.0
+#define MIN_Z 1.3
 
 void transPoints(const PCRGB &pc_in, const Eigen::Matrix4f &trans, PCRGB &pc_out)
 {
@@ -39,7 +52,7 @@ public:
   PCCylinderRemoval(ros::NodeHandle& nh)
     : nh_(nh)
   {
-    pc_sub_ = nh.subscribe<PCRGB>("pc_in", 1, &PCCylinderRemoval::recvPCCallback, this);
+    pc_sub_ = nh.subscribe<PCRGB>("pc_in", 5, &PCCylinderRemoval::recvPCCallback, this);
     pc_pub_ = nh.advertise<PCRGB>("pc_out", 1);
   }
 protected:
@@ -49,28 +62,65 @@ protected:
   ros::NodeHandle nh_;
   ros::Subscriber pc_sub_;
   ros::Publisher pc_pub_;
+
+  std::vector<mean_var_acc> accs_;
 };
 
 void PCCylinderRemoval::recvPCCallback(const PCRGB::ConstPtr& pc_in)
 {
-  Eigen::Matrix3f tf;
-  tf << -1.0,  0.0,  0.0,
-         0.0, -1.0,  0.0,
-         0.0,  0.0,  1.0;
+  // Eigen::Matrix3f tf;
+  // tf << -1.0,  0.0,  0.0,
+  //        0.0, -1.0,  0.0,
+  //        0.0,  0.0,  1.0;
+  Eigen::Matrix4f tf;
+  tf << -1.0,  0.0,  0.0,  0.0,
+         0.0, -1.0,  0.0,  0.0,
+         0.0,  0.0,  1.0,  0.0,
+         0.0,  0.0,  0.0,  1.0;
   if(!pc_proc_) {
     pc_proc_.reset(new PCRGB(pc_in->width, pc_in->height));
     // pc_proc_.reset(new PCRGB());
     pc_proc_->header.frame_id = pc_in->header.frame_id;
+    accs_.resize(pc_in->size());
   }
   pc_proc_->header.seq = pc_in->header.seq;
   pc_proc_->header.stamp = pc_in->header.stamp;
         
-  //Eigen::MatrixXf pc_in_mat = pc_in->getMatrixXfMap(3, 4, 0);
-  //Eigen::MatrixXf& pc_proc_mat = pc_proc_->getMatrixXfMap(3, 4, 0);
-  //pc_proc_mat = pc_in_mat * tf;
-  pc_proc_->getMatrixXfMap(3, 4, 0) = tf * pc_in->getMatrixXfMap(3, 4, 0);
-  //ROS_INFO("PTS SIZE %d", pc_proc_->points.size());
-  pc_pub_.publish(pc_proc_);
+  // pc_proc_->getMatrixXfMap(4, 4, 0) = tf * pc_in->getMatrixXfMap(4, 4, 0);
+
+  for(int i = 0; i < pc_in->size(); ++i) {
+    float z = pc_in->at(i).z;
+    if(z != z || z < MIN_Z || z > MAX_Z)
+      continue;
+    accs_[i](z);
+  }
+
+  int num_good_pts = 0;
+  for(int i = 0; i < pc_in->size(); ++i) 
+    if(boost::accumulators::extract::count(accs_[i]) >= 5)
+      num_good_pts++;
+  if(num_good_pts == 0)
+    return;
+  Eigen::MatrixXf A(num_good_pts,3);
+  //Eigen::MatrixXf A(num_good_pts,2);
+  //Eigen::MatrixXf A(num_good_pts,1);
+  Eigen::VectorXf b(num_good_pts);
+  int cur_ind = 0;
+  for(int i = 0; i < pc_in->size(); ++i) {
+    if(boost::accumulators::extract::count(accs_[i]) < 5) 
+      continue;
+    float mean_val = mean(accs_[i]);
+    A(cur_ind,0) = mean_val*mean_val;
+    A(cur_ind,1) = mean_val;
+    A(cur_ind,2) = 1.0f;
+    b(cur_ind) = sqrt(variance(accs_[i]));
+    cur_ind++;
+  }
+  Eigen::VectorXf lin_fit = A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
+  //ROS_INFO("Lin fit: %f", lin_fit[0]);
+  // ROS_INFO("Lin fit: %f, %f", lin_fit[0], lin_fit[1]);
+  ROS_INFO("Lin fit: %f, %f, %f", lin_fit[0], lin_fit[1], lin_fit[2]);
+  // pc_pub_.publish(pc_proc_);
 }
 
 int main( int argc, char** argv )
