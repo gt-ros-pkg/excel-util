8650ab082d7e8da16a10f058f952d70fba297eea
JimmyDaSilva <jimdasilva@hotmail.fr>
2014-09-10 21:43:46 -0400

    Updating the GripperCommand action

--- excel_move/src/excel_move/gripper_action_server.py
+++ excel_move/src/excel_move/gripper_action_server.py
@@ -17,7 +17,7 @@ class GripperAction(object):
   _result   = control_msgs.msg.GripperCommandResult()
 
   def __init__(self, name):
-    self._action_name = 'gripper_action'
+    self._action_name = 'gripper_controller/gripper_action'
     self._as = actionlib.SimpleActionServer(self._action_name, control_msgs.msg.GripperCommandAction, execute_cb=self.execute_cb, auto_start = False)
     self._as.start()
     self.pub = rospy.Publisher('CModelRobotOutput', outputMsg.CModel_robot_output)
@@ -38,22 +38,7 @@ class GripperAction(object):
     self._feedback.effort = gripper_command_msg.command.max_effort
     self._feedback.stalled = True
     self._feedback.reached_goal = True
-    # publish info to the console for the user
-    #rospy.loginfo('%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
-    
-    # start executing the action
-#     for i in xrange(1, goal.order):
-#       # check that preempt has not been requested by the client
-#       if self._as.is_preempt_requested():
-#         rospy.loginfo('%s: Preempted' % self._action_name)
-#         self._as.set_preempted()
-#         success = False
-#         break
-#       self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
-#       # publish the feedback
-#       self._as.publish_feedback(self._feedback)
-#       # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-#       r.sleep()
+
     
     self.pub.publish(command)
     
