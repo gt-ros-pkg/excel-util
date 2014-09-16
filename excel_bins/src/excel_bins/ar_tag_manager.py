e3aa4c5c306b748be66d59bb5f69ffd9c0c134d0
JimmyDaSilva <jimdasilva@hotmail.fr>
2014-09-16 15:22:57 -0400

    Fixing the tag manager/Adding a node to merge 2 tag topics

--- excel_bins/src/excel_bins/ar_tag_manager.py
+++ excel_bins/src/excel_bins/ar_tag_manager.py
@@ -24,7 +24,7 @@ class ARTagManager(ARTagManagerInterface):
         self.camera_pose = PoseConv.to_homo_mat(camera_pos, camera_quat)
         
         # FILTER SIZE HAS TO BE ODD because of the median
-        self.filter_size = 3
+        self.filter_size = 5
         
         self.clean_period = 0.05
         self.last_clean_pub = 0.
@@ -74,6 +74,9 @@ class ARTagManager(ARTagManagerInterface):
                         if not bin_data[bid][:2]:
                             print "empty"
                             break;
+                        if not bin_data[bid+10][:2]:
+                            print "empty"
+                            break;
                         
                         # Get the average position
                         ar_pose1 = PoseConv.to_homo_mat(bin_data[bid][:2])
@@ -85,34 +88,45 @@ class ARTagManager(ARTagManagerInterface):
                         bin.name = "bin#"+str(bid)
                         
                         # Correct bin height
-                        if ar_pose[2,3] > self.table_height+(self.bin_small_height+self.bin_large_height)/2:
-                            ar_pose[2,3] = self.table_height+self.bin_large_height
+                        if (bid%2):
+                            ar_pose[2,3] = self.table_height + self.bin_large_height
                             bin.size = "large"
                         else:
-                            ar_pose[2,3] = self.table_height+self.bin_small_height
+                            ar_pose[2,3] = self.table_height + self.bin_small_height
                             bin.size = "small"
-                        
+#                         # Correct bin height
+#                         if ar_pose[2,3] > self.table_height+(self.bin_small_height+self.bin_large_height)/2:
+#                             ar_pose[2,3] = self.table_height+self.bin_large_height
+#                             bin.size = "large"
+#                         else:
+#                             ar_pose[2,3] = self.table_height+self.bin_small_height
+#                             bin.size = "small"
+                    
                         # Invert rotation
                         pose_msg = PoseConv.to_pose_msg(self.camera_pose**-1 * ar_pose)
-                        pose_msg1 = PoseConv.to_pose_msg(self.camera_pose**-1 * ar_pose1)
-                        pose_msg2 = PoseConv.to_pose_msg(self.camera_pose**-1 * ar_pose2)
-                        
-                        # Get the 2 angles from quaternions
-                        euler1 = euler_from_quaternion([pose_msg1.orientation.w,pose_msg1.orientation.x,pose_msg1.orientation.y,pose_msg1.orientation.z])
-                        euler2 = euler_from_quaternion([pose_msg2.orientation.w,pose_msg2.orientation.x,pose_msg2.orientation.y,pose_msg2.orientation.z])
-                        ang1 = euler1[0]
-                        ang2 = euler2[0]
-                        
-                        # Correct angle range
-                        diff = ang2 - ang1
-                        if (diff<0):
-                            diff = diff + 2*math.pi
-                        if (diff>math.pi/2):
-                            diff = -(2*math.pi-diff)
-                        ang = ang1+diff/2
+#                         pose_msg1 = PoseConv.to_pose_msg(self.camera_pose**-1 * ar_pose1)
+#                         pose_msg2 = PoseConv.to_pose_msg(self.camera_pose**-1 * ar_pose2)
+#                         
+#                         # Get the 2 angles from quaternions
+#                         euler1 = euler_from_quaternion([pose_msg1.orientation.w,pose_msg1.orientation.x,pose_msg1.orientation.y,pose_msg1.orientation.z])
+#                         euler2 = euler_from_quaternion([pose_msg2.orientation.w,pose_msg2.orientation.x,pose_msg2.orientation.y,pose_msg2.orientation.z])
+#                         ang1 = euler1[0]
+#                         ang2 = euler2[0]
+#                         
+#                         # Correct angle range
+#                         diff = ang2 - ang1
+#                         if (diff<0):
+#                             diff = diff + 2*math.pi
+#                         if (diff>math.pi/2):
+#                             diff = -(2*math.pi-diff)
+#                         ang = ang1+diff/2
+#                         
+                        xdiff = ar_pose1[0,3] - ar_pose2[0,3] 
+                        ydiff = ar_pose1[1,3] - ar_pose2[1,3]
+                        ang = np.arctan2(xdiff, ydiff)
 
                         # Get quaternion from angle
-                        new_quaternion = quaternion_from_euler(ang-math.pi/2, 0, 0)
+                        new_quaternion = quaternion_from_euler(ang+math.pi, 0, 0)
                         pose_msg.orientation.x = new_quaternion[1]
                         pose_msg.orientation.y = new_quaternion[2]
                         pose_msg.orientation.z = new_quaternion[3]
