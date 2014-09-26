b66d2f2c792f6a58eebeadb1ed61737259419a2d
JimmyDaSilva <jimdasilva@hotmail.fr>
2014-09-26 18:09:09 -0400

    Node to scan the 3 parts on the table

---
+++ excel_move/src/scanning.cpp
@@ -0,0 +1,179 @@
+#include "scanning.h"
+
+/*--------------------------------------------------------------------
+ * Scanning()
+ * Constructor.
+ *------------------------------------------------------------------*/
+Scanning::Scanning() : group("excel"), excel_ac("vel_pva_trajectory_ctrl/follow_joint_trajectory") ,spinner(1)
+{
+	spinner.start();
+	boost::shared_ptr<tf::TransformListener> tf(new tf::TransformListener(ros::Duration(2.0)));
+	planning_scene_monitor::PlanningSceneMonitorPtr plg_scn_mon(new planning_scene_monitor::PlanningSceneMonitor("robot_description", tf));
+	planning_scene_monitor = plg_scn_mon;
+
+	ros::NodeHandle nh_, nh_param_("~");
+	nh_param_.getParam("sim",sim);
+
+	ros::WallDuration sleep_t(0.5);
+	group.setPlanningTime(8.0);
+	group.allowReplanning(false);
+
+	service_client = nh_.serviceClient<moveit_msgs::GetPositionIK> ("compute_ik");
+	while(!service_client.exists())
+	{
+		ROS_INFO("Waiting for IK service");
+		sleep(1.0);
+	}
+	fk_client = nh_.serviceClient<moveit_msgs::GetPositionFK> ("compute_fk");
+	while(!fk_client.exists())
+	{
+		ROS_INFO("Waiting for FK service");
+		sleep(1.0);
+	}
+
+	// Loading planning_scene_monitor //
+	planning_scene_monitor->startSceneMonitor();
+	planning_scene_monitor->startStateMonitor();
+	planning_scene_monitor->startWorldGeometryMonitor();
+	planning_scene_monitor->requestPlanningSceneState();
+	full_planning_scene = planning_scene_monitor->getPlanningScene();
+	full_planning_scene->getPlanningSceneMsg(planning_scene);
+
+	// Define joint_constraints for the IK service //
+	rail_constraint.joint_name = "table_rail_joint";
+	rail_constraint.position = 2.0;
+	rail_constraint.tolerance_above = 1.3;
+	rail_constraint.tolerance_below = 1.6;
+	rail_constraint.weight = 1;
+	shoulder_constraint.joint_name = "shoulder_lift_joint";
+	shoulder_constraint.position = -M_PI/2;
+	shoulder_constraint.tolerance_above = M_PI/4;
+	shoulder_constraint.tolerance_below = 0;
+	shoulder_constraint.weight = 1;
+
+	// Config the IK service //
+	service_request.ik_request.group_name = "scanning";
+	service_request.ik_request.pose_stamped.header.frame_id = "table_link";
+	service_request.ik_request.avoid_collisions = true;
+	service_request.ik_request.attempts = 30;
+	service_request.ik_request.constraints.joint_constraints.push_back(rail_constraint);
+	service_request.ik_request.constraints.joint_constraints.push_back(shoulder_constraint);
+
+	// no try yet //
+	orientation_try = 0;
+
+	// start with orientation 0 //
+	current_orientation = 0;
+}
+
+int Scanning::scan(int pose, int orientation){
+	if (pose==0){
+		service_request.ik_request.pose_stamped.pose.position.x = 0.60;
+		service_request.ik_request.pose_stamped.pose.position.y = 1.94;
+		service_request.ik_request.pose_stamped.pose.position.z = 0.95;
+	}
+	if (pose==1){
+		service_request.ik_request.pose_stamped.pose.position.x = 0.52;
+		service_request.ik_request.pose_stamped.pose.position.y = 2.11;
+		service_request.ik_request.pose_stamped.pose.position.z = 0.95;
+	}
+	if (pose==2){
+		service_request.ik_request.pose_stamped.pose.position.x = 0.48;
+		service_request.ik_request.pose_stamped.pose.position.y = 2.00;
+		service_request.ik_request.pose_stamped.pose.position.z = 0.95;
+	}
+
+	tf::Quaternion quat;
+	if (orientation==0){
+		quat = tf::createQuaternionFromRPY(-M_PI/2,0.9,-M_PI/2);
+	}
+	if (orientation==1){
+		quat = tf::createQuaternionFromRPY(-M_PI/2,0.9,-M_PI/2-M_PI/5);
+	}
+	if (orientation==2){
+		quat = tf::createQuaternionFromRPY(-M_PI/2,0.9,-M_PI/2-M_PI/3);
+	}
+	service_request.ik_request.pose_stamped.pose.orientation.x = quat.x();
+	service_request.ik_request.pose_stamped.pose.orientation.y = quat.y();
+	service_request.ik_request.pose_stamped.pose.orientation.z = quat.z();
+	service_request.ik_request.pose_stamped.pose.orientation.w = quat.w();
+
+	service_client.call(service_request, service_response);
+	if(service_response.error_code.val !=1){
+		ROS_ERROR("IK couldn't find a solution for step 1");
+	}
+
+	planning_scene_monitor->requestPlanningSceneState();
+	full_planning_scene = planning_scene_monitor->getPlanningScene();
+	full_planning_scene->getPlanningSceneMsg(planning_scene);
+	
+	// Fixing shoulder_pan & wrist_3 given by the IK
+	service_response.solution.joint_state.position[1] = this->optimal_goal_angle(service_response.solution.joint_state.position[1],planning_scene.robot_state.joint_state.position[1]);
+	service_response.solution.joint_state.position[6] = this->optimal_goal_angle(service_response.solution.joint_state.position[6],planning_scene.robot_state.joint_state.position[6]);
+	group.setStartState(full_planning_scene->getCurrentState());
+	group.setJointValueTarget(service_response.solution.joint_state);
+
+	if(group.plan(my_plan)){
+		group.execute(my_plan);
+		sleep(1.0);
+	}else ROS_ERROR("Planning failed");
+	
+	int ok;
+	std::cout << "Do you see pose " << pose<< " at orientation "<< orientation<< " ?"<<std::endl;
+	std::cin >> ok;
+	
+	if (ok==2) ros::shutdown();
+	
+	return ok;
+}
+
+/*--------------------------------------------------------------------
+ * optimal_goal_angle()
+ * Finds out if the robot needs to rotate clockwise or anti-clockwise
+ *------------------------------------------------------------------*/
+double Scanning::optimal_goal_angle(double goal_angle, double current_angle)
+{
+	while( std::abs(std::max(current_angle,goal_angle) - std::min(current_angle,goal_angle))>M_PI){
+		if (goal_angle>current_angle) goal_angle -= 2*M_PI;
+		else goal_angle += 2*M_PI;
+	}
+	if(goal_angle>2*M_PI) goal_angle -= 2*M_PI;
+	if(goal_angle<-2*M_PI) goal_angle += 2*M_PI;
+	return goal_angle;
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "scanning");
+	usleep(1000*1000);
+	
+	Scanning scanning;
+	std::cout << "-----------------------------" << std::endl;
+	std::cout << "USE THE NUMBER 2 TO SHUTDOWN" << std::endl;
+	std::cout << "-----------------------------" << std::endl;
+	while(ros::ok()){
+		while(scanning.orientation_try<4){
+			if(scanning.scan(0, scanning.current_orientation)) break;
+			scanning.current_orientation = (scanning.current_orientation +1) % 3;
+			scanning.orientation_try += 1;
+		}
+		scanning.orientation_try = 0;
+
+		while(scanning.orientation_try<4){
+			if(scanning.scan(1, scanning.current_orientation)) break;
+			scanning.current_orientation = (scanning.current_orientation +1) % 3;
+			scanning.orientation_try += 1;
+		}
+		scanning.orientation_try = 0;
+
+		while(scanning.orientation_try<4){
+			if(scanning.scan(2, scanning.current_orientation)) break;
+			scanning.current_orientation = (scanning.current_orientation +1) % 3;
+			scanning.orientation_try += 1;
+		}
+		scanning.orientation_try = 0;
+	}
+
+	ros::shutdown();
+	return 0;
+}
---
+++ excel_move/src/scanning.h
@@ -0,0 +1,66 @@
+#ifndef SCANNING_H
+#define SCANNING_H
+
+// ROS/MoveIt includes.
+#include "ros/ros.h"
+#include "ros/time.h"
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+#include <moveit_msgs/JointConstraint.h>
+#include <moveit_msgs/GetPositionIK.h>
+#include <moveit_msgs/GetPositionFK.h>
+#include <moveit_msgs/RobotTrajectory.h>
+#include <moveit_msgs/RobotState.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <tf/transform_broadcaster.h>
+#include <moveit/robot_state/attached_body.h>
+#include "geometric_shapes/mesh_operations.h"
+#include "geometric_shapes/shape_operations.h"
+#include <shape_msgs/Mesh.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <control_msgs/GripperCommandAction.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include "tf/transform_datatypes.h"
+
+# define M_PI 3.14159265358979323846  /* pi */
+# define TABLE_HEIGHT 0.88  
+# define GRIPPING_OFFSET 0.1  
+# define DZ 0.25 
+
+class Scanning
+{
+public:
+	// Constructor.
+	Scanning();
+	
+	double optimal_goal_angle(double goal_angle, double current_angle);
+	int scan(int pose, int orientation);
+
+	ros::ServiceClient service_client, fk_client;
+	moveit_msgs::GetPositionIK::Request service_request;
+	moveit_msgs::GetPositionIK::Response service_response;
+	moveit_msgs::GetPositionFK::Request fk_request;
+	moveit_msgs::GetPositionFK::Response fk_response;
+
+	planning_scene::PlanningScenePtr full_planning_scene;
+	moveit_msgs::PlanningScene planning_scene;
+	planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor;	
+	boost::shared_ptr<tf::TransformListener> tf;
+	move_group_interface::MoveGroup group;
+	ros::AsyncSpinner spinner;
+	actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> excel_ac;
+	moveit_msgs::JointConstraint rail_constraint, shoulder_constraint,elbow_constraint;
+	moveit::planning_interface::MoveGroup::Plan my_plan;
+	double rail_max, rail_min, rail_tolerance;
+	bool sim;
+	int orientation_try, current_orientation;
+};
+
+#endif
